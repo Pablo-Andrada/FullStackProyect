97c115e03ef6e6c743e1455d16cb34e714d5bc68
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-10-19 01:16:34 -0300

    nueva funcionalidad al title cuando cambia pestaña

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -70,3 +70,17 @@ function buildMovie(movie) {
 function buildMovies() {
     return tempData.map((movie) => buildMovie(movie));
 }
+
+
+//Funcionalidad al title al cambiar la pestaña
+
+let previousTitle = document.title;
+
+window.addEventListener('blur', () => {
+    previousTitle = document.title
+    document.title = '¡No te vayas! ¡Vuelve!'
+})
+
+window.addEventListener('focus', () => {
+    document.title = previousTitle;
+})
