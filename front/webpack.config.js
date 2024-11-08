bbf4b884f79d2efed9c0df1a72d97bfdad40fd80
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-11-08 06:01:17 -0300

    acomodando webpack

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: "./scripts/index.js",
     output: {
-        //path: __dirname + 'dist'
+        //path: __dirname + '/dist'
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
       },
