8dbdf33fa8d6d23b61dea6f46509cc87317b3a6c
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-12-22 21:49:50 -0300

    Release 1.0.1

--- back/src/config/dbCon.js
+++ back/src/config/dbCon.js
@@ -1,9 +1,27 @@
+// require("dotenv").config();
+// const mongoose = require("mongoose");
+
+
+// const dbCon = async () => {
+//     await mongoose.connect(process.env.MONGO_URI);
+//  };
+ 
+//  module.exports = dbCon;
+
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-
 const dbCon = async () => {
-    await mongoose.connect(process.env.MONGO_URI);
- };
- 
- module.exports = dbCon;
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Conexión exitosa a la base de datos");
+    } catch (error) {
+        console.error("Error al conectar a la base de datos", error);
+        throw error; // Lanzar el error para que sea manejado en el lugar donde se llama la función
+    }
+};
+
+module.exports = dbCon;
