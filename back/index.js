38ad6e7b9ab5e72865caaa4ee7d3f3c568949bb1
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-12-22 22:24:17 -0300

    Release 1.0.3

--- back/index.js
+++ back/index.js
@@ -1,3 +1,15 @@
+// const app = require("./src/server");
+// const dbCon = require("./src/config/dbCon");
+
+// dbCon().then(() => {
+//     app.listen(3000, () => {
+//         console.log("Servidor escuchando en el puerto 3000");
+//     })
+// })
+//     .catch((error) => {
+//     console.log("Error al conectar a la base de datos");    
+// })
+
 const app = require("./src/server");
 const dbCon = require("./src/config/dbCon");
 
@@ -9,4 +21,3 @@ dbCon().then(() => {
     .catch((error) => {
     console.log("Error al conectar a la base de datos");    
 })
-
--- back/src/config/dbCon.js
+++ back/src/config/dbCon.js
@@ -8,19 +8,41 @@
  
 //  module.exports = dbCon;
 
-require("dotenv").config();
-const mongoose = require("mongoose");
+// require("dotenv").config();
+// const mongoose = require("mongoose");
+
+// const dbCon = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+//         console.log("Conexión exitosa a la base de datos");
+//     } catch (error) {
+//         console.error("Error al conectar a la base de datos", error);
+//         throw error; // Lanzar el error para que sea manejado en el lugar donde se llama la función
+//     }
+// };
+
+// module.exports = dbCon;
+const mongoose = require('mongoose');
 
 const dbCon = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        const uri = process.env.MONGO_URI;
+        if (!uri) {
+            throw new Error("La variable de entorno MONGO_URI no está definida.");
+        }
+
+        await mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
+
         console.log("Conexión exitosa a la base de datos");
     } catch (error) {
-        console.error("Error al conectar a la base de datos", error);
-        throw error; // Lanzar el error para que sea manejado en el lugar donde se llama la función
+        console.error("Error al conectar a la base de datos:", error.message);
+        throw error; // Lanza el error para que lo capture el índice
     }
 };
 
